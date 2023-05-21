a9fb42f346f34c99a288d55444b16229d9887d11
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-05-21 16:39:33 +0600

    c++ module 1

---
+++ c++_output.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ cout<<"hello"<<endl<<"world"<<endl;
+ int a=100,b=200;
+ cout<<a<<" "<<b;
+
+    return 0;
+}
---
+++ c++_switch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;    
+    cin>>a;
+    switch (a)
+    {
+    case 1:
+        cout<<"one"<<endl;
+        break;
+    case 2:
+        cout<<"two"<<endl;
+        break;
+    case 3:
+        cout<<"three"<<endl;
+        break;
+    case 4:
+        cout<<"four"<<endl;
+        break;
+    case 5:
+        cout<<"five"<<endl;
+        break;
+    
+    default:
+       cout<<"didn't match";
+        break;
+    }   
+
+    return 0;
+}
