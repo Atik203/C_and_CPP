ce6cc21df28f5c7a05679202f3188abf3df5d6b4
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-08-31 11:13:47 +0600

    Algorithm module 18

---
+++ C++/DSU_cycle_detect.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+int parent[1000];
+int parentLevel[1000];
+void dsu_set(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = -1;
+        parentLevel[i] = 0;
+    }
+}
+
+int find(int node)
+{
+    while (parent[node] != -1)
+    {
+        node = parent[node];
+    }
+    return node;
+}
+
+void dsu_union(int a, int b)
+{
+    int leaderA = find(a);
+    int leaderB = find(b);
+    if (leaderA != leaderB)
+    {
+        if (parentLevel[leaderA] > parentLevel[leaderB])
+        {
+            parent[leaderB] = leaderA;
+        }
+        else if (parentLevel[leaderA] < parentLevel[leaderB])
+        {
+            parent[leaderA] = leaderB;
+        }
+        else
+        {
+            parent[leaderB] = leaderA;
+            parentLevel[leaderA]++;
+        }
+    }
+}
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    dsu_set(n);
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int leaderA = find(a);
+        int leaderB = find(b);
+        if (leaderA == leaderB)
+        {
+            cout << "Cycle detected : " << a << " " << b << endl;
+        }
+        else
+        {
+            dsu_union(a, b);
+        }
+    }
+
+    return 0;
+}
