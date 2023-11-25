2c06a5f01ec709e2f9fda0feb36e1559fb0141ae
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-11-25 23:52:46 +0600

    codeforcees

---
+++ C++/B_AB_Flipping.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define endl '\n'
+#define f(i, s, n) for (long long int i = s; i < n; i++)
+#define cf(i, s, n) for (long long int i = s; i <= n; i++)
+#define rf(i, s, n) for (long long int i = n - 1; i >= s; i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int count = 0, ans = 0;
+        rf(i, 0, n)
+        {
+            if (s[i] == 'B')
+            {
+                count++;
+            }
+            else
+            {
+                ans += count;
+                if (count)
+                {
+
+                    s[i] = 'B';
+                    count = 0;
+                    i++;
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
