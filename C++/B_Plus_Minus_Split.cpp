492611be7d523cf77cbd87c8501cdad74c2bbef9
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-01-06 22:44:13 +0600

    hello 2024 codeforces

---
+++ C++/B_Plus_Minus_Split.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define endl '\n'
+#define f(i, s, n) for (long long int i = s; i < n; i++)
+#define cf(i, s, n) for (long long int i = s; i <= n; i++)
+#define rf(i, s, n) for (long long int i = n - 1; i >= s; i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define all(a) sort(a.begin(), a.end())
+#define All(a, n) sort(a, a + n);
+#define rall(a) sort(a.rbegin(), a.rend())
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int s1 = 0, s2 = 0;
+
+    f(i, 0, n)
+    {
+        if (s[i] == '+')
+        {
+            s1++;
+        }
+        else
+            s2++;
+    }
+    int a = max(s1, s2);
+    int b = min(s1, s2);
+    int result = a - b;
+    cout << result << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
